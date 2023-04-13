aa435813396fd0c2737f79337b58aab6360e74ef
Joonas Lindstedt <68861164+jslindgit@users.noreply.github.com>
2023-04-14 00:14:20 +0300

    10.16 valmis

--- rate-repository-app/src/components/AppBar.jsx
+++ rate-repository-app/src/components/AppBar.jsx
@@ -1,6 +1,11 @@
 import { ScrollView, StyleSheet, View } from "react-native";
 import { Link } from "react-router-native";
 
+import { useQuery } from "@apollo/client";
+import { ME } from "../graphql/queries";
+import { useAuthStorage } from "../hooks/useAuthStorage";
+import { useApolloClient } from "@apollo/client";
+
 import Constants from "expo-constants";
 
 import Text from "./Text";
@@ -24,15 +29,48 @@ const styles = StyleSheet.create({
 });
 
 const AppBar = () => {
+	const authStorage = useAuthStorage();
+	const apolloClient = useApolloClient();
+
+	const { data, error, loading } = useQuery(ME, {
+		fetchPolicy: "cache-and-network",
+	});
+
+	if (error) {
+		console.log("AppBar.jsx error:", error);
+	}
+	if (loading) {
+		return (
+			<View>
+				<Text>Loading...</Text>
+			</View>
+		);
+	}
+	console.log("AppBar data:", data);
+
+	const signOut = async () => {
+		await authStorage.removeAccessToken();
+		apolloClient.resetStore();
+	};
+
+	const signIn =
+		data.me !== null ? (
+			<Text color="textLight" onPress={signOut}>
+				Sign out ({data.me.username})
+			</Text>
+		) : (
+			<Link to="/signin" style={styles.flexItem}>
+				<Text color="textLight">Sign in</Text>
+			</Link>
+		);
+
 	return (
 		<View style={styles.flexContainer}>
 			<ScrollView horizontal>
 				<Link to="/" style={styles.flexItem}>
 					<Text color="textLight">Repositories</Text>
 				</Link>
-				<Link to="/signin" style={styles.flexItem}>
-					<Text color="textLight">Sign in</Text>
-				</Link>
+				{signIn}
 			</ScrollView>
 		</View>
 	);
